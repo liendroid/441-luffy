d0062cccb0d9bb70b27282c4085f0c8a50d46adb
liendroid <littleassassin_001@hotmail.com>
2016-03-24 12:40:02 -0600

    Created some Gui
    Need to figure out how to switch between panels or frames. Will need
    more time.

---
+++ Connect Four/gui/GameLobby.java
@@ -0,0 +1,47 @@
+package gui;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+
+public class GameLobby extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					GameLobby frame = new GameLobby();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public GameLobby() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 692, 548);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblGameLobby = new JLabel("Game lobby!");
+		lblGameLobby.setBounds(163, 144, 73, 14);
+		contentPane.add(lblGameLobby);
+	}
+
+}
