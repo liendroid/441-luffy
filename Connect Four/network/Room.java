b0c1a435ecb096b0c619eb8c88c8420633d3eb47
Dan <dan.kiese@gmail.com>
2016-03-29 20:53:43 -0600

    Added random things that room will prob store

--- Connect Four/network/Room.java
+++ Connect Four/network/Room.java
@@ -1,4 +1,7 @@
 package network;
+
+import gameLogic.GameBoard;
+
 /* just hold the room information
  * 
  */
@@ -7,6 +10,9 @@ public class Room {
 	String player2 = "";
 	int turn = 1; //(can be 1 or 2)
 	boolean joinable = true;
+	String[] spectators;
+	String[] roomMessages;
+	GameBoard board;
 	
 	Room(String player){
 		player1 = player;
